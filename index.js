061ba8a5d2e345f53c20c6cecd9e538cdd7a2bc5
Jorge <plaxiola19@gmail.com>
2025-05-30 12:34:55 -0700

    primer commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+// Cargar datos desde el archivo JSON
+let kardexList = [];
+try {
+    const data = fs.readFileSync(__dirname + 'kardexDb.json', 'utf8');
+    kardexList = JSON.parse(data); 
+} catch (error) {
+    console.error('Error al leer el archivo kardexList.json', error);
+}
+
+//Ruta para obtener kardex
+app.get('/kardex',(req, res) => {
+    res.json(kardexList);
+})
+
+// Ruta para obtener un alumno por número de cuenta
+app.get('/kardex/:numeroCuenta', (req, res) => {
+  const alumno = alumnos.find(a => a.numero_cuenta === req.params.numeroCuenta);
+  if (alumno) {
+    res.json(alumno);
+  } else {
+    res.status(404).json({ mensaje: "Alumno no encontrado" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`API corriendo en http://localhost:${port}`);
+});
